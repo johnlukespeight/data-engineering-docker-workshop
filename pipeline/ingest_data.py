6f69327145a10286174e4324dcae3f34b4c4f02a
John-Luke Speight <johnlukespeight@gmail.com>
2026-01-26 18:59:31 +0000

    Multiple Docker Containers

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -5,6 +5,7 @@ from os import name
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 dtype = {
     "VendorID": "Int64",
@@ -28,26 +29,37 @@ dtype = {
 parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
 
 
-def run():
-    pg_user = "root"
-    pg_pass = "root"
-    pg_host = "localhost"
-    pg_port = "5432"
-    pg_db = "ny_taxi"
-
-    year = 2021
-    month = 1
-
-    target_table = "yellow_taxi_data"
-
-    chunk_size = 100000
-
-    prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
-    url = f"{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz"
+@click.command()
+@click.option("--pg-user", default="root", help="PostgreSQL username")
+@click.option("--pg-pass", default="root", help="PostgreSQL password")
+@click.option("--pg-host", default="localhost", help="PostgreSQL host")
+@click.option("--pg-port", default="5432", help="PostgreSQL port")
+@click.option("--pg-db", default="ny_taxi", help="PostgreSQL database name")
+@click.option("--year", default=2021, type=int, help="Year of the data")
+@click.option("--month", default=1, type=int, help="Month of the data")
+@click.option("--chunk-size", default=100000, type=int, help="Chunk size for ingestion")
+@click.option("--target-table", default="yellow_taxi_data", help="Target table name")
+def run(
+    pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunk_size, target_table
+):
 
     engine = create_engine(
         f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
     )
+    prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
+
+    url = f"{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz"
+    year = 2021
+    month = 1
+    chunk_ize = 100000
+    target_table = "yellow_taxi_data"
+
+    engine = create_engine(
+        f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
+    )
+    prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
+
+    url = f"{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz"
 
     df_iter = pd.read_csv(
         url, dtype=dtype, parse_dates=parse_dates, iterator=True, chunksize=chunk_size
